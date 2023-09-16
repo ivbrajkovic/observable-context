8d97cc0ddde15db9984a99d3e4310cc53fcd11ca
Ivan Brajković <ivan.brajkovic@icloud.com>
2023-09-16 17:47:33 +0200

    add element support

--- src/components/ObservableTracker/ObservableTracker.tsx
+++ src/components/ObservableTracker/ObservableTracker.tsx
@@ -1,17 +1,59 @@
-import { ElementRef, useEffect, useRef, useState } from "react";
-import classes from "./ObservableTracker.module.css";
 import { Observable, SubscriberData } from "class/Observable";
+import { cloneDeep } from "lodash-es";
+import { ElementRef, useEffect, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 
-function ObservableTracker<T extends Record<string, unknown>>(props: {
+import classes from "components/ObservableTracker/ObservableTracker.module.css";
+
+type WithEventListeners = {
+  addEventListener: (type: string, listener: EventListener) => void;
+  removeEventListener: (type: string, listener: EventListener) => void;
+};
+
+function hasEventListeners(obj: unknown): obj is WithEventListeners {
+  return typeof obj === "object" && obj !== null && "addEventListener" in obj;
+}
+
+function elementToObject(el: HTMLInputElement) {
+  return {
+    tagName: el.tagName,
+    id: el.id,
+    type: el.type,
+    name: el.name,
+    value: el.value,
+  };
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function parseObject(obj: Record<string, any>) {
+  for (const p in obj) {
+    // eslint-disable-next-line no-prototype-builtins
+    if (obj.hasOwnProperty(p)) {
+      if (hasEventListeners(obj[p])) {
+        obj[p] = elementToObject(obj[p]);
+      } else if (typeof obj[p] === "object") {
+        obj[p] = parseObject(obj[p]);
+      }
+    }
+  }
+  return obj;
+}
+
+function ObservableTracker<T extends Record<string, unknown>>({
+  hasElement,
+  useObservableContext,
+}: {
+  hasElement?: boolean;
   useObservableContext: () => Observable<T>;
 }) {
-  const observable = props.useObservableContext();
+  const observable = useObservableContext();
 
   const observableTrackerRef = useRef<ElementRef<"article">>(null);
   const observableTrackerHeaderRef = useRef<ElementRef<"header">>(null);
 
-  const [state, setState] = useState(() => ({ ...observable.observed }));
+  const [state, setState] = useState(() => {
+    return cloneDeep(observable.observed);
+  });
   const [subscribers, setSubscribers] = useState<SubscriberData[]>([]);
 
   useEffect(() => {
@@ -25,11 +67,38 @@ function ObservableTracker<T extends Record<string, unknown>>(props: {
   }, [observable]);
 
   useEffect(() => {
-    const subscription = observable.subscribeAll((key, value) =>
-      setState((state) => ({ ...state, [key]: value })),
-    );
+    const subscription = observable.subscribeAll(() => {
+      setState(cloneDeep(observable.observed));
+    });
     return subscription;
   }, [observable]);
+
+  // const [elements] = useState<Set<Element>>(() => new Set());
+  useEffect(() => {
+    if (!hasElement) return;
+    const handleChange = () => setState(cloneDeep(observable.observed));
+    const elements = new Set<WithEventListeners>();
+    const deepForEach = (obj: Record<string, unknown>) => {
+      for (const p in obj) {
+        // eslint-disable-next-line no-prototype-builtins
+        if (obj.hasOwnProperty(p)) {
+          const element = obj[p];
+          if (hasEventListeners(element)) {
+            elements.add(element);
+            element.addEventListener("input", handleChange);
+          } else if (typeof element === "object" && element !== null) {
+            deepForEach(element as Record<string, unknown>);
+          }
+        }
+      }
+    };
+    deepForEach(observable.observed);
+    return () => {
+      elements.forEach((element) => {
+        element.removeEventListener("input", handleChange);
+      });
+    };
+  }, [hasElement, observable.observed]);
 
   useEffect(() => {
     if (!observableTrackerRef.current) return;
@@ -84,7 +153,7 @@ function ObservableTracker<T extends Record<string, unknown>>(props: {
           <main>
             <section>
               <p>State:</p>
-              <pre>{JSON.stringify(state, null, 2)}</pre>
+              <pre>{JSON.stringify(parseObject(state), null, 2)}</pre>
             </section>
 
             <section>
